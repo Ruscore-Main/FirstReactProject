5a5b4d6e76d23fc569928956d8fcfd2cc996dbf0
Ruscore-Main <oobitovich@mail.ru>
2021-01-13 22:49:36 +0300

    created base css

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ function App() {
   return (
   <div className='container'>
     <header>
+      <span>fadfafa</span>
+      <span>fsfadf</span>
+      <span>fdsaf</span>
+      <span>fdafd</span>
+      <div className="img">
       <img src="https://st2.depositphotos.com/3096625/8799/v/600/depositphotos_87990570-stock-illustration-letter-r-logo-monogram.jpg" alt="logo"/>
+      <span>
+        Rnet
+      </span>
+      </div>
     </header>
+    <div className="content">
     <nav>
       <ul>
         <li>Profile</li>
@@ -17,8 +27,9 @@ function App() {
       </ul>
     </nav>
     <main>
-      Main content
+      Main contentProfile
     </main>
+    </div>
     {/* <Header />
     <Technologies /> */}
   </div>
