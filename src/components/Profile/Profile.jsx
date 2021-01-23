3b3b26e1c13a566d4f446d1e387087c13e892f90
Ruscore-Main <oobitovich@mail.ru>
2021-01-23 17:09:12 +0300

    checking

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,6 +7,7 @@ const Profile = () => {
         <main>
             <img src="https://png.pngtree.com/thumb_back/fw800/background/20190827/pngtree-retro-futuristic-1980s-style-mountain-landscape-background-image_302222.jpg" alt=""/>
             <p>Main contentProfile</p>
+            <p>after main content</p>
             <MyPost />
         </main>
 
